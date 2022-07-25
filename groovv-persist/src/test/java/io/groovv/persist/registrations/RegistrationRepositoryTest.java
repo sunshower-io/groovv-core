e8efa56922f397443de96ffcfcd2914b306d9f3c
Josiah Haswell <josiah.haswell@gmail.com>
2022-07-25 16:21:41 -0600

    Adding realm

--- groovv-persist/src/test/java/io/groovv/persist/registrations/RegistrationRepositoryTest.java
+++ groovv-persist/src/test/java/io/groovv/persist/registrations/RegistrationRepositoryTest.java
@@ -3,6 +3,7 @@ package io.groovv.persist.registrations;
 import static io.sunshower.model.test.RegistrationTests.createValidRequest;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import io.groovv.model.api.registrations.Realm;
 import io.groovv.persist.PersistenceTest;
 import javax.inject.Inject;
 import org.junit.jupiter.api.Test;
@@ -16,5 +17,12 @@ public class RegistrationRepositoryTest {
   void ensureSavingValidRegistrationWorks() {
     repository.save(createValidRequest());
     assertEquals(1L, repository.count());
+  }
+
+  @Test
+  void ensureQueryingByRealmWorks() {
+    repository.save(createValidRequest());
+    assertEquals(1, repository.findByRealm(Realm.Google).size());
+    assertEquals(0, repository.findByRealm(Realm.Groovv).size());
   }
 }
