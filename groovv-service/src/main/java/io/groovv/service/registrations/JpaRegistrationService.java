47e3af8cc91395fea74a997a4f14097ab18e8d04
Josiah Haswell <josiah.haswell@gmail.com>
2022-07-25 16:06:02 -0600

    Adding account service.  Creating SDK project

---
+++ groovv-service/src/main/java/io/groovv/service/registrations/JpaRegistrationService.java
@@ -0,0 +1,5 @@
+package io.groovv.service.registrations;
+
+public class JpaRegistrationService implements RegistrationService {
+
+}
---
+++ groovv-service/src/main/java/io/groovv/service/registrations/RegistrationService.java
@@ -0,0 +1,5 @@
+package io.groovv.service.registrations;
+
+public interface RegistrationService {
+
+}
