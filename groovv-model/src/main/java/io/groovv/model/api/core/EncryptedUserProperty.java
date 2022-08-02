ca3d342b0eac4bf77743860bb2c167a9a13d441e
Josiah Haswell <josiah.haswell@gmail.com>
2022-08-01 18:33:59 -0600

    Adding encrypted user property

---
+++ groovv-model/src/main/java/io/groovv/model/api/core/EncryptedUserProperty.java
@@ -0,0 +1,20 @@
+package io.groovv.model.api.core;
+
+import io.sunshower.persistence.id.Identifier;
+import io.sunshower.persistence.id.Identifiers;
+import io.sunshower.persistence.id.Sequence;
+
+public class EncryptedUserProperty extends
+    EncryptedProperty<Identifier, User, EncryptedUserProperty> {
+
+  static final Sequence<Identifier> SEQUENCE;
+
+  static {
+    SEQUENCE = Identifiers.newSequence(true);
+  }
+
+
+  public EncryptedUserProperty() {
+    super(SEQUENCE.next());
+  }
+}
